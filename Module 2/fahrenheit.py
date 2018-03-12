42f6ccf205780cbc8e4ad63c2461c094454fd377
philipohagan <phil.ohagan@apperio.com>
2018-03-12 15:12:27 +0000

    changes for python3

--- Module 2/fahrenheit.py
+++ Module 2/fahrenheit.py
@@ -1,7 +1,7 @@
 def fahrenheit(celsius):
     return celsius * 9/5 + 32
 
-temperature = int(raw_input("Enter Temperature: "))
+temperature = int(input("Enter Temperature in Celsius: "))
 if temperature < -273.15:
     print ("Too cold")
 else:
